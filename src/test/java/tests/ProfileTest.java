9e0e93a4094da2d472e4889ae6264b84ac3a7655
sarthak-choudhari <76088641+sarthak-choudhari@users.noreply.github.com>
2022-03-09 13:27:44 +0530

    Fn

---
+++ src/test/java/tests/ProfileTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import java.awt.HeadlessException;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pageObjects.initializePageObjects.PageFactoryInitializer;
+
+public class ProfileTest extends PageFactoryInitializer {
+
+	@BeforeMethod
+	public void applicationLogin() {
+		homePage().clickOnAccountIcon();
+		homePage().clickOnSignIn();
+		loginPage().setUsername(EMAIL_ID);
+		loginPage().setPassword(PASSWORD);
+		loginPage().clickSubmit();
+	}
+
+	@Test(priority = 1)
+	public void C2387_profileVerificationStatusValidation() {
+
+		homePage().clickOnAccountIcon();
+		homePage().click(homePage().profile);
+		profilePage().verifyProfileVerificationStatus();
+
+	}
+
+	@Test(priority = 2)
+	public void C2389_passwordChangeValidation() {
+
+		homePage().clickOnAccountIcon();
+		homePage().click(homePage().profile);
+		profilePage().verifyChangePassword();
+	}
+
+	@Test(priority = 3)
+	public void C2391_referalLinkValidation() throws HeadlessException, UnsupportedFlavorException, IOException {
+
+		homePage().clickOnAccountIcon();
+		homePage().click(homePage().profile);
+		profilePage().verifyreferralLink();
+	}
+
+	@Test(priority = 4)
+	public void C2395_accountActivityValidation() {
+
+		homePage().clickOnAccountIcon();
+		homePage().click(homePage().profile);
+		profilePage().verifyAccountActivities();
+	}
+	
+	@Test(priority = 5)
+	public void dummyTest() throws IOException {
+
+		homePage().clickOnAccountIcon();
+		homePage().click(homePage().profile);
+		throw new IOException("Errrrrrrrrrrrooooorrrrrr");
+	}
+}
